3560931781c7c5e1f2f40a347e91920df1053bd7
ll870524 <ll870524@163.com>
2016-04-28 11:14:41 +0800

    初来乍到

---
+++ AimarCodeProj/AimarCodeProj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AimarCodeProj
+//
+//  Created by 立李 on 16/4/28.
+//  Copyright © 2016年 李立. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AimarCodeProj/AimarCodeProj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AimarCodeProj
+//
+//  Created by 立李 on 16/4/28.
+//  Copyright © 2016年 李立. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
